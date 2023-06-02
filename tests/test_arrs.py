0b6a7bed6bc53e348dbee3b47969f3996218f50b
lija_lyfe <lija6556@list.ru>
2023-06-02 16:49:10 +0500

    add def test_my_slice_negative_start_and_end_indices

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -23,8 +23,6 @@ class TestArrs(unittest.TestCase):
         arr = [1, 2, 3, 4, 5]
         self.assertEqual(my_slice(arr, 1, 4), [2, 3, 4])
 
-
-
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    def test_my_slice_negative_start_and_end_indices(self):
+        arr = [1, 2, 3, 4, 5]
+        self.assertEqual(my_slice(arr, -3, -1), [3, 4])
