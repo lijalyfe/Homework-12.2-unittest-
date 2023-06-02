bf116d11defa9f0a11c624269fb7801547c99f0b
lija_lyfe <lija6556@list.ru>
2023-06-02 16:37:38 +0500

    add class TestArrs

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,6 +2,7 @@ import unittest
 from arrs import arrs
 
 
+class TestArrs(unittest.TestCase):
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 3
     assert arrs.get([], 0, "test") == "test"
